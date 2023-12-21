b6975e7343d835fbf40199814ed612362b09d022
Mohammad Ali peyrovi <peyrovi98@gmail.com>
2023-12-21 19:37:33 +0330

    Hello git.

---
+++ app/src/main/java/com/github/peyrovi98/popupdialog/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.peyrovi98.popupdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
